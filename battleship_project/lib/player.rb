b783de8ab8c833278ac520726fce3af8e066c090
Vera Ho <vh71886@gmail.com>
2022-04-05 16:40:35 -0400

    Battleship project completed. Passes all specs.

--- battleship_project/lib/player.rb
+++ battleship_project/lib/player.rb
@@ -1,4 +1,10 @@
 class Player
-    
-
+    def get_move
+        p "enter a position with coordinates separated by a space like '4 7'"
+        input = gets.chomp.split(" ")
+        input.each.with_index do |num, i|
+            input[i] = num.to_i
+        end
+        input
+    end
 end
