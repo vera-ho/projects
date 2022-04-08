3df80792e6273f2268517acfabeb6158e4f674a4
Vera Ho <vh71886@gmail.com>
2022-04-08 17:21:56 -0400

    added print, up and down methods

--- todoboard/list.rb
+++ todoboard/list.rb
@@ -46,6 +46,60 @@ class List
     end
 
     def print
-        puts "list stuff here"
+        puts "Index |  Deadline   |  Item"
+        @items.each.with_index do |item, i|
+            puts "#{i}     |  #{item.deadline} |  #{item.title}"
+        end
     end
+
+    def print_full_item(idx)
+        if valid_index?(idx)
+            puts "#{self[idx].title}  |  #{self[idx].deadline} | #{self[idx].description}"
+        end
+    end
+
+    def print_priority
+        print_full_item(0)
+    end
+
+    # playing with recursion
+    def up(idx, amt=1)
+        if valid_index?(idx)
+            if amt == 1      # up(0,1) OK due to valid_index? check in swap
+                swap(idx, idx - 1)
+            elsif idx > 0
+                swap(idx, idx - 1)
+                up(idx - 1, amt - 1)
+            end
+            return true
+        end
+        false
+    end
+
+    # def down(idx, amt)
+    #     if valid_index?(idx)
+    #         if amt == 1
+    #             swap(idx, idx + 1)
+    #         elsif idx < @items.length
+    #             swap(idx, idx + 1)
+    #             down(idx + 1, amt - 1)
+    #         end
+    #         return true
+    #     end
+    #     false
+    # end
+
+    def down(idx, amt=1)
+        if valid_index?(idx)
+            while amt > 0
+                swap(idx, idx + 1)
+                idx += 1
+                amt -= 1
+            end
+            return true
+        end
+        false
+    end
+
+    
 end
