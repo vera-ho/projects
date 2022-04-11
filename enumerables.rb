292ce9a48da9d9d90ec91bcaeec7e1d1f5b0c937
Vera Ho <vh71886@gmail.com>
2022-04-11 10:58:32 -0400

    initial commit

---
+++ enumerables.rb
@@ -0,0 +1,39 @@
+def my_each(&prc)
+
+end
+
+def my_select(&prc)
+
+end
+
+def my_reject
+
+end
+
+def my_any?
+
+end
+
+def my_all?
+
+end
+
+def my_flatten
+
+end
+
+def my_zip
+
+end
+
+def my_rotate
+
+end
+
+def my_join
+
+end
+
+def my_reverse
+
+end
