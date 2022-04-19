ae7720cd20631a5e7ab3822af04d53b9db45d097
Vera Ho <vh71886@gmail.com>
2022-04-18 22:06:59 -0400

    work in progress

--- tictactoeAI/lib/super_computer_player.rb
+++ tictactoeAI/lib/super_computer_player.rb
@@ -2,6 +2,16 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    new_node=TicTacToeNode.new(game.board,mark)
+    new_node.children.each do |child|
+        if child.winning_node?(mark)
+          return child.prev_move_pos
+        end
+    end
+    new_node.children.each do |node|
+      return node if !node.losing_node?(mark)
+    end
+
   end
 end
 
@@ -12,3 +22,4 @@ if __FILE__ == $PROGRAM_NAME
 
   TicTacToe.new(hp, cp).run
 end
+
