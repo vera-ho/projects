80a8cd30dd87d3c060c27bd4b977441205a804c1
Madeleine Pla <madeleine.pla7@gmail.com>
2022-04-27 11:21:53 -0400

    Complete find_by_ids

--- w5d3/aa_questions/questions_db.rb
+++ w5d3/aa_questions/questions_db.rb
@@ -61,15 +61,75 @@ class Question
 end
 
 class QuestionFollow
+    attr_accessor :id, :user_id, :question_id
 
+    def self.find_by_id(question_follow_id)
+        question_follow = QuestionsDatabase.instance.execute(<<-SQL, question_follow_id)
+            SELECT
+                *
+            FROM
+                question_follows
+            WHERE
+                id = ?
+        SQL
+        return nil unless question_follow.length > 0
+        # p user
+        QuestionFollow.new(question_follow.first)
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @question_id = options['question_id']
+        @user_id = options['user_id']
+    end
 end
 
 class Reply
+    attr_accessor :id, :question_id, :parent_reply_id, :user_id, :body
 
+    def self.find_by_id(reply_id)
+        reply = QuestionsDatabase.instance.execute(<<-SQL, reply_id)
+            SELECT
+                *
+            FROM
+                replies
+            WHERE
+                id = ?
+        SQL
+        return nil unless reply.length > 0
+        Reply.new(reply.first)
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @question_id = options['question_id']
+        @parent_reply_id = options['parent_reply_id']
+        @user_id = options['user_id']
+        @body = options['body']
+    end
     
 end
 
 class QuestionLike
+    attr_accessor :id, :question_id, :user_id
 
+    def self.find_by_id(question_like_id)
+        question_like = QuestionsDatabase.instance.execute(<<-SQL, question_like_id)
+            SELECT
+                *
+            FROM
+                question_likes
+            WHERE
+                id = ?
+        SQL
+        return nil unless question_like.length > 0
+        QuestionLike.new(question_like.first)
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @question_id = options['question_id']
+        @user_id = options['user_id']
+    end
 
 end
